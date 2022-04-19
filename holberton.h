70cfccfe8fc8ffbd5aa0aaba20e802f644bde5ff
TesfayeAdugna <adugnatesfaye888@gmail.com>
2022-04-19 19:49:20 +0300

    adding holberton file

---
+++ holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#define NULL ((void *)0)
+
+int _putchar(char c);
+int _print_str(char *string);
+int _print_int(long int var);
+int _printf(const char *format, ...);
+int to_Binary(unsigned int n);
+int to_Octal(unsigned int n);
+int _power_recursion(int x, int y);
+int to_Hexa(unsigned int num);
+
+#endif
